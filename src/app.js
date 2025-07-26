5c4227605d3709a82981f89b1e247ba9e2bc3645
ameeenamzz <shoukethameen@gmail.com>
2025-07-26 22:59:04 +0530

    explored error handling

--- src/app.js
+++ src/app.js
@@ -5,22 +5,33 @@ app.listen(7777, () => {
 });
 
 //------MIDDLEWARE--------
-const { adminAuth, uesrAuth } = require("./middlewares/auth");
+// const { adminAuth, uesrAuth } = require("./middlewares/auth");
 
-app.use("/admin", adminAuth);
+// app.use("/admin", adminAuth);
 
-app.get("/admin/getAllData", (req, res) => {
-  res.send("All Data sent...");
+// app.get("/admin/getAllData", (req, res) => {
+//   res.send("All Data sent...");
+// });
+
+// app.get("/admin/deleteAllData", (req, res) => {
+//   res.send("Deleted all data");
+// });
+
+// app.get("/user/getAlldata", uesrAuth, (req, res) => {
+//   res.send("user data sent");
+// });
+
+// app.post("/user/login", (req, res) => {
+//   res.send("login sucussfull");
+// });
+
+app.get("/user", (req, res, next) => {
+  throw new Error("erorororor");
+  res.send("helo");
 });
 
-app.get("/admin/deleteAllData", (req, res) => {
-  res.send("Deleted all data");
-});
-
-app.get("/user/getAlldata", uesrAuth, (req, res) => {
-  res.send("user data sent");
-});
-
-app.post("/user/login", (req, res) => {
-  res.send("login sucussfull");
+app.use("/", (err, req, res, next) => {
+  if (err) {
+    res.status(500).send("something went wrond!!!!");
+  }
 });
