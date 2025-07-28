fce752016b3b0bb4d7442e2bda495f4337d5531e
ameeenamzz <shoukethameen@gmail.com>
2025-07-28 22:05:42 +0530

    Refactor user-related routes: add delete and update functionality, improve comments

--- src/app.js
+++ src/app.js
@@ -5,9 +5,9 @@ const User = require("./models/user");
 
 app.use(express.json());
 
+// ADDING USERS TO DATABASE
 app.post("/signup", async (req, res) => {
   const user = new User(req.body); //CREATING A NEW INSTANCE OF USER MODEL
-
   try {
     await user.save(); //ADDING THE DATA TO DATABASE
     res.send("User Added Successfully");
@@ -18,7 +18,7 @@ app.post("/signup", async (req, res) => {
 });
 
 // GETTING USERS FROM DATABASE
-// gettting one user
+// a) gettting one user
 app.get("/user", async (req, res) => {
   const userEmail = req.body.email;
   try {
@@ -33,7 +33,7 @@ app.get("/user", async (req, res) => {
   }
 });
 
-// getting all users
+// b) getting all users
 app.get("/feed", async (req, res) => {
   try {
     const allUsers = await User.find({});
@@ -43,6 +43,30 @@ app.get("/feed", async (req, res) => {
       res.send(allUsers);
     }
   } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+// DELETE USER
+
+app.delete("/userDelete", async (req, res) => {
+  const deleteUser = req.body.firstName;
+  try {
+    const user = await User.deleteOne({ firstName: deleteUser });
+    res.send("User Deleted Sucessfully!");
+  } catch (error) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+// UPDATING USER DETAILS
+
+app.patch("/userUpdate", async (req, res) => {
+  const { id, ...user } = req.body;
+  try {
+    const updateUser = await User.findByIdAndUpdate(id, user);
+    res.send("User updated Succesfully");
+  } catch (error) {
     res.status(400).send("Something went wrong");
   }
 });
