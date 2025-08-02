69c79a192791158c78674b8ec1d3767f1ea8cd51
ameeenamzz <shoukethameen@gmail.com>
2025-08-02 12:40:11 +0530

    Add connection request model and implement send connection request route

--- src/routes/requestRouter.js
+++ src/routes/requestRouter.js
@@ -1,14 +1,33 @@
 const express = require("express");
 const requestRouter = express.Router();
 const { userAuth } = require("../middlewares/auth");
+const ConnectionRequest = require("../models/connectionRequest");
 
-requestRouter.get("/sendConnectionRequest", userAuth, async (req, res) => {
-  try {
-    const user = req.user;
-    res.send(user.lastName);
-  } catch (err) {
-    res.status(404).send("ERROR: " + err.message);
+requestRouter.post(
+  "/request/send/:status/:toUserId",
+  userAuth,
+  async (req, res) => {
+    try {
+      // const user = req.user;
+      const fromUserId = req.user._id;
+      const toUserId = req.params.toUserId;
+      const status = req.params.status;
+      const connectionRequest = new ConnectionRequest({
+        fromUserId,
+        toUserId,
+        status,
+      });
+      await connectionRequest.save();
+      res.send(
+        fromUserId.firstName,
+        "sent connection request to",
+        toUserId.firstName
+      );
+      // res.send("connection send successfully");
+    } catch (err) {
+      res.status(404).send("ERROR: " + err.message);
+    }
   }
-});
+);
 
 module.exports = requestRouter;
