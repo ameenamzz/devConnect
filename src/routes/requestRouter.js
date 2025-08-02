f3a2ae72cf711bda42411f3adaca2719384c4300
ameeenamzz <shoukethameen@gmail.com>
2025-08-02 13:43:54 +0530

    Refactor request sending logic: streamline user retrieval and response message

--- src/routes/requestRouter.js
+++ src/routes/requestRouter.js
@@ -2,14 +2,15 @@ const express = require("express");
 const requestRouter = express.Router();
 const { userAuth } = require("../middlewares/auth");
 const ConnectionRequest = require("../models/connectionRequest");
+const User = require("../models/user");
 
 requestRouter.post(
   "/request/send/:status/:toUserId",
   userAuth,
   async (req, res) => {
     try {
-      // const user = req.user;
-      const fromUserId = req.user._id;
+      const user = req.user;
+      const fromUserId = user._id;
       const toUserId = req.params.toUserId;
       const status = req.params.status;
       const connectionRequest = new ConnectionRequest({
@@ -17,12 +18,11 @@ requestRouter.post(
         toUserId,
         status,
       });
+
+      const toUser = await User.findById(toUserId);
+      console.log(toUser);
       await connectionRequest.save();
-      res.send(
-        fromUserId.firstName,
-        "sent connection request to",
-        toUserId.firstName
-      );
+      res.send("request sent successfully");
       // res.send("connection send successfully");
     } catch (err) {
       res.status(404).send("ERROR: " + err.message);
