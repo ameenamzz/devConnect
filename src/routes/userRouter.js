79697219f677af7035544bc0986e782b666696dc
ameeenamzz <shoukethameen@gmail.com>
2025-08-06 22:57:49 +0530

    implemented pagination in feed api

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -56,6 +56,10 @@ userRouter.get("/user/connection", userAuth, async (req, res) => {
 
 userRouter.get("/feed", userAuth, async (req, res) => {
   const loggedInUser = req.user;
+  const page = parseInt(req.query.page) || 1;
+  let limit = parseInt(req.query.limit) || 10;
+  limit = limit > 50 ? 50 : limit;
+  const skip = (page - 1) * limit;
 
   const connectionRequest = await ConnectionRequest.find({
     $or: [{ toUserId: loggedInUser._id }, { fromUserId: loggedInUser._id }],
@@ -72,7 +76,10 @@ userRouter.get("/feed", userAuth, async (req, res) => {
       { _id: { $nin: Array.from(hideUserFromFeed) } },
       { _id: { $ne: loggedInUser._id } },
     ],
-  }).select("firstName lastName age skills photoURL");
+  })
+    .select("firstName lastName age skills photoURL")
+    .skip(skip)
+    .limit(limit);
 
   res.send(users);
 });
