e0feac3e27a938cf4e845e5888d29718ff06b69f
ameeenamzz <shoukethameen@gmail.com>
2025-08-19 14:58:53 +0530

    removed ses email

--- src/routes/requestRouter.js
+++ src/routes/requestRouter.js
@@ -55,11 +55,6 @@ requestRouter.post(
 
       await connectionRequest.save();
 
-      const emailRes = await sendEmail.run(
-        "Connection Request Update",
-        `Your connection request has been ${status} by ${user.firstName}.`
-      );
-
       res.json({
         message: status + "successfully",
       });
